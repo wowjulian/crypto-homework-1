8e82f02c0c8aa1b3f71ec3caf612b863305c80c9
Julian <julian@linkcruit.com>
2025-01-20 20:28:01 -0800

    Add gcd function

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,34 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// Name of the person to greet
+    /// An integer a, which is the modulus
     #[arg(short, long)]
-    name: String,
+    a: i64,
 
-    /// Number of times to greet
+    /// non-negative integer b that is less than a.
     #[arg(short, long)]
-    count: u8,
+    b: i64,
+}
+
+fn gcd(a: i64, b: i64) -> i64 {
+    let reminder = a % b;
+    if a % b == 0 {
+        return b;
+    }
+    return gcd(b, reminder);
 }
 
 fn main() {
     let args = Args::parse();
-
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name);
+    if args.b < 0 {
+        println!("b must be non-negative integer");
+        return;
     }
+    if args.b >= args.a {
+        println!("b must be less than a");
+        return;
+    }
+
+    let gcd_result = gcd(args.a, args.b);
+    println!("gcd: {}", gcd_result);
 }
