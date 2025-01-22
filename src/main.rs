afc0f55848146364545a128e713d1ba03d4f811e
Julian <julian@linkcruit.com>
2025-01-21 21:28:12 -0800

    Working x and y

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
 
-/// Simple program to greet a person
+/// Simple program to calculate gcd and stuff
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
@@ -14,17 +14,49 @@ struct Args {
 }
 
 fn gcd(a: i64, b: i64) -> i64 {
-    let reminder = a % b;
-    if a % b == 0 {
-        return b;
+    if b == 0 {
+        return a;
     }
-    return gcd(b, reminder);
+    return gcd(b, a % b);
+}
+
+fn print_x_and_y(a: i64, b: i64) {
+    let mut x_prev2: i64 = 1;
+    let mut x_prev1: i64 = 0;
+    let mut y_prev2: i64 = 0;
+    let mut y_prev1: i64 = 1;
+
+    let mut remainder_prev2 = a;
+    let mut remainder_prev1 = b;
+    let mut current_remainder: i64 = -1;
+
+    while current_remainder != 0 {
+        let floored_quotient = remainder_prev2 / remainder_prev1;
+        current_remainder = remainder_prev2 - (remainder_prev1 * floored_quotient);
+        if current_remainder == 0 {
+            break;
+        }
+
+        let current_x: i64 = x_prev2 - (floored_quotient * x_prev1);
+        let current_y = y_prev2 - (floored_quotient * y_prev1);
+
+        x_prev2 = x_prev1;
+        x_prev1 = current_x;
+
+        y_prev2 = y_prev1;
+        y_prev1 = current_y;
+
+        remainder_prev2 = remainder_prev1;
+        remainder_prev1 = current_remainder;
+    }
+
+    println!("x: {} and y: {}", x_prev1, y_prev1);
 }
 
 fn main() {
     let args = Args::parse();
-    if args.b <= 0 {
-        println!("b must be non-negative, non-zero integer");
+    if args.b < 0 {
+        println!("b must be non-negative");
         return;
     }
     if args.b >= args.a {
@@ -34,4 +66,5 @@ fn main() {
 
     let gcd_result = gcd(args.a, args.b);
     println!("gcd: {}", gcd_result);
+    print_x_and_y(args.a, args.b);
 }
